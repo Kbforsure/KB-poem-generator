bb4b77a28e457ff057cf338cb76e93e5540481e3
kbforsure <katieannablair@gmail.com>
2025-05-12 01:16:12 -0700

    adding api elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("Poem generated!");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
 }
 
-new Typewriter("#poem", {
-  strings: "Hello, there!",
-  autoStart: true,
-  delay: 3,
-  cursor: "",
-});
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "do8e05a55a39a74t37328f1154eb845f";
+  let prompt = "";
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating Poem...");
+  axios.get(apiUrl).then(displayPoem);
+}
 
 let peomFormElement = document.querySelector("#poem-generator-form");
 peomFormElement.addEventListener("submit", generatePoem);
