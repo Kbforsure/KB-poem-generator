d3b3ff49aaff36969263e932c7d39f392794a038
kbforsure <katieannablair@gmail.com>
2025-05-12 00:51:15 -0700

    testing typewriter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 function generatePoem(event) {
-  alert("hello");
+  event.preventDefault();
 }
+
+new Typewriter("#poem", {
+  strings: "Hello, there!",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
 
 let peomFormElement = document.querySelector("#poem-generator-form");
 peomFormElement.addEventListener("submit", generatePoem);
