51ee0c6cbe73873165ee5969d238208c6d5c2259
kbforsure <katieannablair@gmail.com>
2025-05-12 01:27:41 -0700

    testing api response

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
+
   let apiKey = "do8e05a55a39a74t37328f1154eb845f";
-  let prompt = "";
-  let context = "";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a funny poem expert who loves to write short poems about farts! Your mission is to generate a 4 line poem in basic HTML. Make sure to include the prompt AND farts below. Please behave.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Generating Poem...");
   axios.get(apiUrl).then(displayPoem);
