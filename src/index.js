da1f775a66bbd35242a8416baadbfbfd391e286b
kbforsure <katieannablair@gmail.com>
2025-05-12 02:01:35 -0700

    tweaking display

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generatePoem(event) {
   let apiKey = "do8e05a55a39a74t37328f1154eb845f";
   let prompt = `Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a sweet poem generator who produces concise yet romantic poems about farts, please be sure to include a fart joke as part of the poem. Please keep the poem as short as possible. Sign the poem with 'SheCodes AI' inside a <strong> element";
+    "You are a sweet poem generator who produces concise yet romantic poems about farts, please be sure to include a fart joke as part of the poem. Please keep the poem as short as possible. Sign the poem with 'SheCodes AI' after a inside a <strong> element after a <br>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Generating Poem...");
 
